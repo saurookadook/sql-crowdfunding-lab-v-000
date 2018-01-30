aa02a58258e44d93784da499e15609fe6240bd96
Andy Maskiell <maskiella@gmail.com>
2018-01-30 16:33:02 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT,
+);
+
+CREATE TABLE
+
+CREATE TABLE
